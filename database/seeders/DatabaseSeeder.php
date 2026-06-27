922b38e688c597c51448714b800f176203c859a0
Nzicode <Nazari.Moradkhani@gmail.com>
2026-06-27 23:48:44 +0330

    feat: add service_types table and seeder with initial service definitions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
             BranchOfficeSeeder::class,
             StaffUnitSeeder::class,
             UserSeeder::class,
+            ServiceTypeSeeder::class,
         ]);
     }
 }
